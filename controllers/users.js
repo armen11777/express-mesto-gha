116cf034e21d8567f368ade1b8c687b756002cbf
Армен Погосян <armen-pogosyan@mail.ru>
2023-06-13 00:41:32 +0300

    ПР13_1.4

--- controllers/users.js
+++ controllers/users.js
@@ -13,13 +13,13 @@ const getUsers = (req, res) => {
 const getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
-      if (user === null) {
-        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
-        return;
-      }
       res.send({ data: user });
     })
     .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+        return;
+      }
       res.status(500).send({ message: err.message });
     });
 };
@@ -42,12 +42,12 @@ const updateUser = (req, res) => {
   User.findByIdAndUpdate(req.user._id, {
     name: nameUser,
     about: aboutUser,
+  }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
   })
     .then((user) => {
-      if (user === null) {
-        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
-        return;
-      }
       res.send({ data: user });
     })
     .catch((err) => {
@@ -63,6 +63,10 @@ const updateAvatar = (req, res) => {
   const { avatarUser } = req.body;
   User.findByIdAndUpdate(req.user._id, {
     avatar: avatarUser,
+  }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
   })
     .then((user) => {
       if (user === null) {
