ec14151716e608df40f3b31c553bb51c7b981acf
Армен Погосян <armen-pogosyan@mail.ru>
2023-06-22 02:33:26 +0300

    ПР14_1.7

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().uri(),
   }),
 }), createUser);
 app.use(auth);
