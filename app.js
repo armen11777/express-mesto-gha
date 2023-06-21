4000bd3de34a14057db368d8b7204ed44d279c75
Армен Погосян <armen-pogosyan@mail.ru>
2023-06-22 01:45:14 +0300

    ПР14_1.3

--- app.js
+++ app.js
@@ -21,15 +21,16 @@ app.use('/*', (req, res) => {
   res.status(NotFound).send({ message: 'Страница не найдена' });
 });
 
-app.use((err, req, res) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send(
-    { message: statusCode === 500 ? 'На сервере произошла ошибка' : message },
-  );
-});
+
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send(
+    { message: statusCode === 500 ? 'На сервере произошла ошибка' : message },
+  );
+});
 app.listen(PORT);
