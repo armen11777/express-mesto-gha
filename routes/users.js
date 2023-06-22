2d5163830bf931062fcfce4d982f8729c3d6d270
Армен Погосян <armen-pogosyan@mail.ru>
2023-06-23 01:07:26 +0300

    ПР14_2.3

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,26 @@ const router = require('express').Router();
 const {
   getUsers, getUser, updateUser, updateAvatar, currentUser,
 } = require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
 
 router.get('/', getUsers);
 router.get('/me', currentUser);
-router.get('/:userId', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', celebrate({
+  // валидируем параметры
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+}), getUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[a-z0-9._~:/?#]+#?/),
+  }),
+}), updateAvatar);
 
 module.exports = router;
