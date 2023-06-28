724041e2a4ee0dbebc6bd4c478d4569abd69a69d
Армен Погосян <armen-pogosyan@mail.ru>
2023-06-29 00:38:12 +0300

    ПР15_1.12

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
